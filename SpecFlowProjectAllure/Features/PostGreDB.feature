276db8bbeed91d78bf4605e17c1c83cadc00e320
Vignesh Parameswari <vky2688@gmail.com>
2021-07-21 11:40:15 +0100

    PostgreSQL Connection Changes

---
+++ SpecFlowProjectAllure/Features/PostGreDB.feature
@@ -0,0 +1,7 @@
+﻿Feature: PostGreSQLDB
+	Simple query to make connection and fetch a result
+
+@db
+Scenario: Fetch a value from db
+	Given I made a connection to db
+	Then I fetch a value from table
---
+++ SpecFlowProjectAllure/Steps/PostGreSQLDBSteps.cs
@@ -0,0 +1,45 @@
+﻿using Npgsql;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProjectAllure.Steps
+{
+    [Binding]
+    public class PostGreSQLDBSteps
+    {
+
+        private static string Host = "localhost";
+        private static string User = "postgres";
+        private static string DBname = "postgres";
+        private static string Password = "admin";
+        private static string Port = "5432";
+
+        [Given(@"I made a connection to db")]
+        public void GivenIMadeAConnectionToDb()
+        {
+
+
+            var cs = "Host=localhost;Username=postgres;Password=admin;Database=postgres";
+
+            using (var con = new NpgsqlConnection(cs))
+            {
+                con.Open();
+                var sql = "SELECT name from links where id=1";
+
+                using (var cmd = new NpgsqlCommand(sql, con))
+                {
+                    String result = cmd.ExecuteScalar().ToString();
+                    Console.WriteLine("result -> " + result);
+                }
+            }
+           
+
+        }
+
+            [Then(@"I fetch a value from table")]
+            public void ThenIFetchAValueFromTable()
+            {
+                Console.WriteLine($"PostgreSQL version:");
+            }
+        }
+    }
